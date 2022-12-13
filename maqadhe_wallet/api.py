4a91f0c6da17451fd7a1f7b660750190bac2210f
sudarsan2001 <sudarsan2001@outlook.com>
2022-12-13 10:55:40 +0000

    new feat

--- maqadhe_wallet/api.py
+++ maqadhe_wallet/api.py
@@ -60,7 +60,6 @@ def get_wallet_transactions():
 	emails = frappe.db.sql('''select distinct(customer_mail) from `tabWallet Balances` ''',as_list=1)
 	emails= [i[0] for i in emails]
 	l=[]
-	print(emails)
 	for i in emails:
 		l.append(i)
 		url = "https://apimaqadhe.ribox.me/rest/all/V1/walletsystem/transaction/"+str(i)
@@ -71,10 +70,13 @@ def get_wallet_transactions():
 		if resp["transactionList"] == "No Data Found!":
 			continue
 		for j in resp["transactionList"]:
-			cwb = frappe.db.sql('''select name from `tabConnector Wallet Transactions`  where customer_mail = %s
-				and transaction_at = %s ''',(j["customer_mail"],j["transaction_at"]),as_dict=1)
+			cwb = frappe.db.sql('''select name from `tabConnector Wallet Transactions`  where email = %s
+				and transaction_at = %s ''',(j["email"],j["transaction_at"]),as_dict=1)
 			if not cwb:
 				j["doctype"] = "Connector Wallet Transactions"
+				j["customer_name"] = j["name"]
+				j["mode_of_payment"] = j["payment_method"]
+				del j["name"]
 				j["api_status"] = j["status"]
 				del j["status"]
 				j["retry_limit"] = 5
@@ -90,16 +92,17 @@ def create_jv_for_transactions(tsn_id = None):
 	if tsn_id:
 		filter  += "and name = '{0}' ".format(tsn_id)
 	cwb = frappe.db.sql('''select sender_type,mode_of_payment,amount,customer_id,name,retry_limit,is_sync,transaction_note from `tabConnector Wallet Transactions` where is_sync = 0
-			and status = "Pending" and action = "credit" and retry_limit > 0 {0} order by creation asc limit 10'''.format(filter),as_dict =1)
+			and status = "Pending" and retry_limit > 0 {0} order by creation asc limit 10'''.format(filter),as_dict =1)
 	pay_mode = frappe.db.get_value("Wallet Settings","Wallet Settings","default_mode_of_payment")
 	for i in cwb:
-		if i["is_sync"]:
+		if i["is_sync"] or not i["amount"]:
 				continue
 		frappe.db.set_value("Connector Wallet Transactions",i["name"],"retry_limit",int(i["retry_limit"])-1)
 		mod_account = ""
 		if i["sender_type"] == "Recharge wallet":
 			mod_account = frappe.db.get_value("Mode of Payment Account",{"parent":pay_mode},"default_account")
-		elif i["sender_type"] == "Admin credit" or i["sender_type"] == "Admin debit":
+		elif i["sender_type"] == "Admin credit" or i["sender_type"] == "Admin debit" or i["sender_type"] == "Refund order":
+			frappe.errprint("Refund order")
 			mod_account = frappe.db.get_value("Wallet Settings","Wallet Settings","adjustment_account")
 		else:
 			continue
@@ -129,15 +132,58 @@ def create_jv_for_transactions(tsn_id = None):
 		})
 		doc.save(ignore_permissions = True)
 		if doc.name:
+			sje = frappe.db.get_value("Wallet Settings","Wallet Settings","submit_journal_entry")
+			if sje == "1":
+				doc.submit()
 			frappe.db.set_value("Connector Wallet Transactions",i["name"],"reference_id",doc.name)
-			if frappe.db.get_value("Wallet Settings","Wallet Settings","submit_journal_entry"):
-				print("submit")
-				try:
-					doc.submit()
-				except Exception as e:
-					frappe.log_error(frappe.get_traceback())
-					continue
 			frappe.db.set_value("Connector Wallet Transactions",i["name"],"is_sync",1)
 			frappe.db.set_value("Connector Wallet Transactions",i["name"],"status","Synced")
 			link = "<a href='/desk#Form/Journal%20Entry/{0}'>{0}</a>".format(doc.name)
 			frappe.msgprint("Transaction synced against {0}".format(link) )
+
+@frappe.whitelist()
+def use_wallet(doc):
+	amount = 0
+	sn = frappe.get_doc("Sales Invoice",doc)
+	for i in sn.sales_order_payment:
+		if i.is_sync and i.used_wallet_amount == 0:
+			frappe.throw("JV already Created")
+		amount = i.used_wallet_amount
+	if amount<0:
+		amount = abs(amount)
+		new_doc = frappe.new_doc("Journal Entry")
+		new_doc.posting_date = frappe.utils.today()
+		customer = sn.customer
+		customer_wallet = frappe.db.get_value("Wallet Settings","Wallet Settings","account")
+		sales_account = frappe.db.get_value("Wallet Settings","Wallet Settings","sales_account")
+		db_amt = cr_amt = 0
+		db_amt = amount
+		new_doc.append("accounts",{
+                        "account":customer_wallet,
+			"party_type":"Customer",
+			"party":customer,
+                        "debit_in_account_currency":db_amt,
+                        "credit_in_account_currency":cr_amt
+                })
+		new_doc.append("accounts",{
+                        "account":sales_account,
+                        "party_type":"Customer",
+                        "party":customer,
+                        "credit_in_account_currency":db_amt,
+                        "debit_in_account_currency":cr_amt,
+                        "reference_type":"Sales Invoice",
+			"reference_name" : doc
+                })
+		new_doc.save(ignore_permissions = True)
+		if new_doc.name:
+			for i in sn.sales_order_payment:
+				i.used_wallet_amount = 0
+				i.is_sync = 1
+			sn.save(ignore_permissions=True)
+			if sn.reference_num:
+				frappe.db.sql('''update `tabConnector Wallet Transactions` set is_sync=1 ,status = "Synced" where sender_type = "Use wallet" and order_increment_id = %s''',sn.reference_num)
+				frappe.db.commit()
+			link = "<a href='/desk#Form/Journal%20Entry/{0}'>{0}</a>".format(new_doc.name)
+			frappe.msgprint("JV created for Wallet Amount - {0}".format(link))
+		return 0
+
--- maqadhe_wallet/maqadhe_wallet/doctype/connector_wallet_transactions/connector_wallet_transactions.js
+++ maqadhe_wallet/maqadhe_wallet/doctype/connector_wallet_transactions/connector_wallet_transactions.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Connector Wallet Transactions', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.add_custom_button(__("Sync Transaction"), function(){
+			if(frm.doc.is_sync == 1){
+        	                frappe.throw("Transaction already synced")
+	                }
 
-	// }
+			frappe.call({
+				'method': 'maqadhe_wallet.api.create_jv_for_transactions',
+				'freeze':true,
+				'args':{
+				   'tsn_id':frm.doc.name
+				},
+				'callback':function(res){
+					if(res.message){
+					}
+				}
+
+
+			})
+		});
+
+	}
 });
